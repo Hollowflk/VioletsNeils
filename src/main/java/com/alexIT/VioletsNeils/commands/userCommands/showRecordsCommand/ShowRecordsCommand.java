302bfd8096dbac43b98def35c2bdc02a9e24dbb1
hollow <hollowflk@gmail.com>
2025-09-26 18:02:29 +0300

    Изменил вывод записей пользователя. Теперь слот который занимает два часа, выводится один раз

--- src/main/java/com/alexIT/VioletsNeils/commands/userCommands/showRecordsCommand/ShowRecordsCommand.java
+++ src/main/java/com/alexIT/VioletsNeils/commands/userCommands/showRecordsCommand/ShowRecordsCommand.java
@@ -5,6 +5,7 @@ import com.alexIT.VioletsNeils.dto.TgUserDto;
 import com.alexIT.VioletsNeils.entity.TimeSlot;
 import com.alexIT.VioletsNeils.enums.UserState;
 import com.alexIT.VioletsNeils.service.TimeSlotService;
+import com.alexIT.VioletsNeils.utils.MonthsAndDaysUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -26,7 +27,7 @@ public class ShowRecordsCommand implements Command {
     private final TimeSlotService timeSlotService;
     private final TelegramClient telegramClient;
     private static final String TIMESLOT_INFO = """
-            День записи: %s
+            День записи: %s %s
             Время записи: %s ч
             Название услуги:%n%s
             Продолжительность: %s%n
@@ -53,25 +54,47 @@ public class ShowRecordsCommand implements Command {
                 .sorted()
                 .toList();
 
-        SendMessage yourRecordText = SendMessage.builder()
-                .chatId(userId)
-                .text("Ваши записи")
-                .build();
-
+        SendMessage yourRecordText;
+        if (timeSlotList.isEmpty()) {
+            yourRecordText = SendMessage.builder()
+                    .chatId(userId)
+                    .text("У вас нет записей")
+                    .build();
+            sendMessage(userId, yourRecordText);
+            return;
+        } else {
+            yourRecordText = SendMessage.builder()
+                    .chatId(userId)
+                    .text("Ваши записи")
+                    .build();
+        }
         sendMessage(userId, yourRecordText);
 
-        for (TimeSlot timeSlot : timeSlotList) {
-            SendMessage msg = SendMessage.builder()
-                    .chatId(userId)
-                    .text(String.format(TIMESLOT_INFO,
-                            timeSlot.getDailyRecord().getDate(),
-                            timeSlot.getTime(),
-                            timeSlot.getService().getName(),
-                            timeSlot.getService().getDuration()))
-                    .build();
-
+        for (int i = 0; i < timeSlotList.size(); i++) {
+            TimeSlot currentRecord = timeSlotList.get(i);
+            if (i > 0) {
+                TimeSlot previousRecord = timeSlotList.get(i - 1);
+                if (currentRecord.getService().getId().equals(previousRecord.getService().getId())
+                        && currentRecord.getDailyRecord().getDate().equals(previousRecord.getDailyRecord().getDate())) {
+                    continue;
+                }
+            }
+            SendMessage msg = createMessage(currentRecord);
             sendMessage(userId, msg);
         }
+    }
+
+    private SendMessage createMessage(TimeSlot timeSlot) {
+        String monthName = MonthsAndDaysUtils.getNameMonth(timeSlot.getDailyRecord().getDate().getMonth().getValue());
+        return SendMessage.builder()
+                .chatId(timeSlot.getUser().getUserId())
+                .text(String.format(TIMESLOT_INFO,
+                        timeSlot.getDailyRecord().getDate().getDayOfMonth(),
+                        MonthsAndDaysUtils.monthGenitiveForms.get(monthName),
+                        timeSlot.getTime(),
+                        timeSlot.getService().getName(),
+                        timeSlot.getService().getDuration()))
+                .build();
     }
 
     private void sendMessage(Long userId, SendMessage message) {
